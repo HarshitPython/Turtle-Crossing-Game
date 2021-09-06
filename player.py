b04471f46149674f5c46b2a20fcf9812197c5b9f
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-06 18:08:49 +0530

    Add files via upload

---
+++ player.py
@@ -0,0 +1,29 @@
+# # Create a turtle player that starts at the bottom of the screen and listenfor the
+# #"Up" keypress to move the turtle north.
+
+# from turtle import Turtle
+
+# STARTING_POSITION = (0, -270)
+# MOVE_DISTANCE = 10
+# FINISH_LINE_Y = 280
+
+
+# class Player(Turtle):
+#     def __init__ (self):
+#         super().__init__()
+#         self.shape("turtle")
+#         self.penup()
+#         self.go_to_start()
+#         self.setheading(90)
+    
+#     def go_up(self):
+#         self.forward(MOVE_DISTANCE)
+
+#     def go_to_start(self):
+#         self.goto(STARTING_POSITION)
+
+#     def finish(self):
+#         if self.ycor() > FINISH_LINE_Y:
+#             return True
+#         else:
+#             return False
