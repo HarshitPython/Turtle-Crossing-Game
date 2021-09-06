4600a108f335298973947e530c371b7ae6066585
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-06 18:10:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-# screen = Screen()
-# screen.setup(width=600, height=600)
-# screen.tracer(0)
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
 
 # player = Player()
 # car_manager = CarManager()
