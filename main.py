b4cd8524f274762761cdbb660465bec3b224b110
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-06 18:11:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ screen.listen()
 screen.onkeypress(player.go_up,"Up")
 
 
-# game_is_on = True
-# while game_is_on:
-#     time.sleep(0.1)
-#     screen.update()
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
     
 #     car_manager.create_car()
 #     car_manager.move_cars()
