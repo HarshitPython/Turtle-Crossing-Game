dc875e0f58201a96db63dde1147b35f87e2ad025
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-07 12:57:42 +0530

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -17,8 +17,8 @@ class CarManager:
         self.all_cars = []
         self.car_speed = STARTING_MOVE_DISTANCE
 
-#     def create_car(self):
-#         random_chance = random.randint(1,6)
+    def create_car(self):
+        random_chance = random.randint(1,6)
 #         if random_chance == 1:
 #             new_car = Turtle("square")
 #             new_car.shapesize(stretch_len=2,stretch_wid=1)
