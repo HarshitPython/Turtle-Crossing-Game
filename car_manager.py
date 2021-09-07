3577d28acff9e0482d96fccb2c46fbcb6d94ab52
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-07 12:58:01 +0530

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -19,14 +19,14 @@ class CarManager:
 
     def create_car(self):
         random_chance = random.randint(1,6)
-#         if random_chance == 1:
-#             new_car = Turtle("square")
-#             new_car.shapesize(stretch_len=2,stretch_wid=1)
-#             new_car.penup()
-#             new_car.color(random.choice(COLORS))
-#             random_y = random.randint(-240,240)
-#             new_car.goto(300, random_y)
-#             self.all_cars.append(new_car)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_len=2,stretch_wid=1)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-240,240)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
 
 #     def move_cars(self):
 #         for car in self.all_cars:
