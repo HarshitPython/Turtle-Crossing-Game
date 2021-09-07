ecbe1a5a92bdf75501470053e6357bb7100aa6f9
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-07 12:56:40 +0530

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -6,9 +6,9 @@
 from turtle import Turtle
 import random
 
-# COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-# STARTING_MOVE_DISTANCE = 5
-# MOVE_INCREMENT = 10
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
 
 
 # class CarManager:
