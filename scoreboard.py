f63b7c6a6e6a229bf09e1177719dafba0e4f8e6c
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-09 12:54:53 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -5,12 +5,12 @@ FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
-#         super().__init__()
-#         self.level = 1
-#         self.hideturtle()
-#         self.penup()
-#         self.goto(-290,250)
-#         self.update_scoreboard()
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-290,250)
+        self.update_scoreboard()
 
     
 #     def update_scoreboard(self):
